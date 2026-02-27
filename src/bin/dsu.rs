7e329af0530322ee42780548b7111c2c222a2776
theroguevigilante <naimish82@proton.me>
2026-02-28 00:53:27 +0530

    Add Disjoint Sort Union Implementation

---
+++ src/bin/dsu.rs
@@ -0,0 +1,46 @@
+#![allow(dead_code)]
+struct DisjointSet {
+    parent: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        let mut parent = vec![0; n];
+        let mut size = vec![1; n];
+        parent.iter_mut().enumerate().for_each(|(i, val)| *val = i);
+        DisjointSet { parent, size }
+    }
+
+    fn find(&mut self, i: usize) -> usize {
+        if self.parent[i] == i {
+            i
+        } else {
+            let root = self.find(self.parent[i]);
+            self.parent[i] = root;
+            root
+        }
+    }
+
+    fn union(&mut self, i: usize, j: usize) -> bool {
+        let root_i = self.find(i);
+        let root_j = self.find(j);
+
+        if root_i == root_j {
+            return false;
+        }
+
+        if self.size[root_i] > self.size[root_j] {
+            self.parent[root_j] = root_i;
+            self.size[root_i] += self.size[root_j];
+        } else {
+            self.parent[root_i] = root_j;
+            self.size[root_j] += self.size[root_i];
+        }
+        true
+    }
+}
+
+fn main() {
+    todo!();
+}
