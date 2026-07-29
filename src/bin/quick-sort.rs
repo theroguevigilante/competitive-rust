7be34027bcb193f65467be7d61fe3f6858283cd8
theroguevigilante <naimish82@proton.me>
2026-07-29 21:26:50 +0530

    Refactor quick sort to use usize

--- src/bin/quick-sort.rs
+++ src/bin/quick-sort.rs
@@ -1,44 +1,49 @@
-struct Range{
-    low: i32,
-    high: i32
+struct Range {
+    low: usize,
+    high: usize,
 }
 
-impl Range{
-    fn new(low: i32, high: i32) -> Range{
-        Range{ low, high }
+impl Range {
+    fn new(low: usize, high: usize) -> Range {
+        Range { low, high }
     }
 }
 
-fn quick_sort(arr:&mut [i32]){
-    fn partition(arr:&mut [i32], r: &Range) -> i32{
+fn quick_sort(arr: &mut [i32]) {
+    fn partition(arr: &mut [i32], r: &Range) -> usize {
         let mut i = r.low;
         let low = r.low;
         let high = r.high;
-        let pivot = arr[high as usize];
-        for j in low..high{
-            if arr[j as usize] <= pivot{
-                arr.swap(i as usize, j as usize);
-                i+=1;
+        let pivot = arr[high];
+        for j in low..high {
+            if arr[j] <= pivot {
+                arr.swap(i, j);
+                i += 1;
             }
         }
-        arr.swap(i as usize, high as usize);
-        return i;
+        arr.swap(i, high);
+        i
     }
-    if arr.len() < 2{
+    if arr.len() < 2 {
         return;
     }
-    let mut stack = vec![Range::new(0, arr.len() as i32 - 1)];
-    while !stack.is_empty(){
-        let r = stack.pop().unwrap();
-        if r.low >= r.high{
+    let mut stack = vec![Range::new(0, arr.len() - 1)];
+    while let Some(r) = stack.pop() {
+        if r.low >= r.high {
             continue;
         }
         let pi = partition(arr, &r);
-        if (pi - 1 - r.low + 1) > 1{
-            stack.push(Range{low: r.low, high: pi - 1});
+        if (pi - r.low) > 1 {
+            stack.push(Range {
+                low: r.low,
+                high: pi - 1,
+            });
         }
-        if (r.high - pi - 1 + 1) > 1{
-            stack.push(Range{low: pi + 1, high: r.high});
+        if (r.high - pi) > 1 {
+            stack.push(Range {
+                low: pi + 1,
+                high: r.high,
+            });
         }
     }
 }
@@ -46,7 +51,7 @@ fn quick_sort(arr:&mut [i32]){
 fn main() {
     let mut v = vec![10, 9, 5, 1, 7];
     quick_sort(&mut v);
-    for i in &v{
+    for i in &v {
         print!("{i} ");
     }
 }
