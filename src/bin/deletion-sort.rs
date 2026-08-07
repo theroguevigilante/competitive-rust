ff16d48cf4e6373011a9f458a27fba9cd6f56179
theroguevigilante <naimish82@proton.me>
2026-08-07 23:14:53 +0530

    Add deletion sort

---
+++ src/bin/deletion-sort.rs
@@ -0,0 +1,32 @@
+#![allow(dead_code)]
+use std::io::{self, Read};
+use std::str::SplitAsciiWhitespace;
+
+struct Scanner<'a> {
+    iter: SplitAsciiWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Self {
+        Self {
+            iter: s.split_ascii_whitespace(),
+        }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+}
+
+fn solve(stdin: &mut Scanner) {
+
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input).unwrap();
+    let mut stdin = Scanner::new(&input);
+    let t = stdin.next::<i32>();
+    for _ in 0..t {}
+}
+
